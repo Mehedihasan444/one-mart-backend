1150221338d24f9aad875a73ac9741b434cf7d08
Mehedihasan444 <mehedihasan67705251@gmail.com>
2024-05-12 02:14:36 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT|5000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
