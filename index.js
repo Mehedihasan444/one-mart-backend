90a78a02891d8cefdb7255920692153b9db83fe5
Mehedihasan444 <mehedihasan67705251@gmail.com>
2024-05-15 01:24:06 +0600

    added all required api routes

--- index.js
+++ index.js
@@ -1,14 +1,132 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require("cors");
+// const jwt = require("jsonwebtoken");
+// const cookieParser=require('cookie-parser')
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT|5000;
+const port = process.env.PORT || 5000;
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// ==========middleware==========
+app.use(
+  cors({
+    origin: ["http://localhost:5173",],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8pmiatd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-// Start the server
+async function run() {
+  try {
+    const users = client.db("OneMart").collection("users");
+    const services = client.db("OneMart").collection("services");
+    const reviews= client.db("OneMart").collection("reviews")
+    // =================== crud operations ======================
+
+  
+    // post user info
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await users.insertOne(user);
+      res.send(result);
+    });
+    // post user info
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await users.insertOne(user);
+      res.send(result);
+    });
+    // post user review
+    app.post("/reviews", async (req, res) => {
+      const user = req.body;
+      const result = await reviews.insertOne(user);
+      res.send(result);
+    });
+
+    // get user info
+    app.get("/services", async (req, res) => {
+   
+      const result = await services.find().toArray();
+      res.send(result);
+    });
+    // get user info
+    app.get("/admin/users", async (req, res) => {
+   
+      const result = await users.find().toArray();
+      res.send(result);
+    });
+    // get user info
+    app.get("/reviews/:category", async (req, res) => {
+   const query={category: req.params.category}
+      const result = await reviews.find(query).toArray();
+      res.send(result);
+    });
+    // get user info
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await users.findOne(query);
+      res.send(result);
+    });
+
+  
+    // checking whether a user admin or not 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = {
+        email: email,
+        role: "admin",
+      };
+      const result = await users.findOne(query);
+      if (result) {
+        res.send({ admin: true });
+      } else {
+        res.send({ admin: false });
+      }
+   
+    });
+// delete a user
+    app.delete("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await users.deleteOne(query);
+      res.send(result);
+    });
+
+    // make a user admin
+    app.patch("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await users.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`backend is running on port ${port}`);
 });
