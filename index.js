836f52749f903d55adcdf5d7120e286bc1a1ee7a
Mehedihasan444 <mehedihasan67705251@gmail.com>
2024-05-15 12:33:11 +0600

    fix service and review api route

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // ==========middleware==========
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://one-mart-frontend.vercel.app/"],
+    origin: ["http://localhost:5173", "https://one-mart-frontend.vercel.app/"],
     credentials: true,
   })
 );
@@ -31,10 +31,10 @@ async function run() {
   try {
     const users = client.db("OneMart").collection("users");
     const services = client.db("OneMart").collection("services");
-    const reviews= client.db("OneMart").collection("reviews")
+    const reviews = client.db("OneMart").collection("reviews")
     // =================== crud operations ======================
 
-  
+
     // post user info
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -55,56 +55,52 @@ async function run() {
     });
 
     // get user info
-    // app.get("/services", async (req, res) => {
-   
-    //   const result = await services.find().toArray();
-    //   res.send(result);
-    // });
-    app.get("/services/:category", async (req, res) => {
-      // Get the category parameter from the request and replace %20 with spaces
-      const category = req.params.category.replace(/%20/g, ' ');
-  
-      // Construct a MongoDB query to match categories with or without spaces
-      const query = { service: { $regex: new RegExp(category.replace(/\s+/g, '\\s*'), 'i') } };
-  
+    app.get("/services", async (req, res) => {
+
+      const result = await services.find().toArray();
+      res.send(result);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id
+
+      const query = { _id: new ObjectId(id) };
+
       try {
-          // Find documents matching the category query
-          const result = await services.find(query).toArray();
-          res.send(result);
+        const result = await services.findOne(query);
+        res.send(result);
       } catch (error) {
-          console.error("Error fetching reviews:", error);
-          res.status(500).send("Error fetching reviews");
+        console.error("Error fetching reviews:", error);
+        res.status(500).send("Error fetching reviews");
       }
-  });
-  
+    });
+
+    
     // get user info
     app.get("/admin/users", async (req, res) => {
-   
+
       const result = await users.find().toArray();
       res.send(result);
     });
     // get user info
-  //   app.get("/reviews/:category", async (req, res) => {
-  //  const query={category: req.params.category}
-  //     const result = await reviews.find(query).toArray();
-  //     res.send(result);
-  //   });
-  app.get("/reviews/:category", async (req, res) => {
-    // Get the category parameter from the request
-    const category = req.params.category;
-    
-    // Construct a MongoDB query to match categories with or without spaces
-    const query = { category: { $regex: new RegExp(category.replace(/\s+/g, '\\s*'), 'i') } };
+    //   app.get("/reviews/:category", async (req, res) => {
+    //  const query={category: req.params.category}
+    //     const result = await reviews.find(query).toArray();
+    //     res.send(result);
+    //   });
+    app.get("/reviews/:id", async (req, res) => {
 
-    try {
-        // Find documents matching the category query
+      const id = req.params.id;
+
+      const query = { serviceId: id };
+
+      try {
         const result = await reviews.find(query).toArray();
         res.send(result);
-    } catch (error) {
+      } catch (error) {
         console.error("Error fetching reviews:", error);
         res.status(500).send("Error fetching reviews");
-    }
-});
+      }
+    });
 
 
     // get user info
@@ -115,7 +111,7 @@ async function run() {
       res.send(result);
     });
 
-  
+
     // checking whether a user admin or not 
     app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
@@ -129,9 +125,9 @@ async function run() {
       } else {
         res.send({ admin: false });
       }
-   
+
     });
-// delete a user
+    // delete a user
     app.delete("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
